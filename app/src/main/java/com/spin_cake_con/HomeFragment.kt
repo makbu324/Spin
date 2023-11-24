bcb58dbc742a4a65111049d7301befc58a5441b6
Jonah <jonahm@bu.edu>
2023-11-24 14:32:34 -0500

    Basic addition of the google maps api

--- app/src/main/java/com/spin_cake_con/HomeFragment.kt
+++ app/src/main/java/com/spin_cake_con/HomeFragment.kt
@@ -12,15 +12,23 @@ import androidx.annotation.Keep
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.aminography.choosephotohelper.ChoosePhotoHelper
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 @Keep
-class HomeFragment: Fragment() {
+class HomeFragment: Fragment(), OnMapReadyCallback {
     companion object {
         const val TAG = "ChooserFragment"
     }
 
     private var choosePhotoHelper: ChoosePhotoHelper? = null
     private val viewModel by activityViewModels<MainViewModel>()
+    private lateinit var mapView: MapView
+    private lateinit var googleMap: GoogleMap
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -39,7 +47,10 @@ class HomeFragment: Fragment() {
         viewModel.setShowLinkIcon(false)
         viewModel.fragmentTag = TAG
 
-        view.findViewById<WebView>(R.id.google_maps).loadUrl("https://mapsdirections.io")
+        mapView = view.findViewById(R.id.mapView)
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
+
 
         view.findViewById<Button>(R.id.camera_button).setOnClickListener {
             choosePhotoHelper?.takePhoto()
@@ -67,5 +78,12 @@ class HomeFragment: Fragment() {
     override fun onSaveInstanceState(outState: Bundle) {
         choosePhotoHelper?.onSaveInstanceState(outState)
         super.onSaveInstanceState(outState)
+    }
+    override fun onMapReady(map: GoogleMap){
+        googleMap = map
+        val location = LatLng(37.0, -122.0)
+        googleMap.addMarker(MarkerOptions().position(location))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 10f))
+
     }
 }
