7599d9f846e6bb2c17b93bc5b2e99a756fec4fd9
makbu324 <angusmak@bu.edu>
2023-11-23 19:23:19 -0500

    camera works

--- app/src/main/java/com/spin_cake_con/HomeFragment.kt
+++ app/src/main/java/com/spin_cake_con/HomeFragment.kt
@@ -1,13 +1,16 @@
 package com.spin_cake_con
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebView
+import android.widget.Button
 import androidx.annotation.Keep
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import com.aminography.choosephotohelper.ChoosePhotoHelper
 
 @Keep
 class HomeFragment: Fragment() {
@@ -15,7 +18,7 @@ class HomeFragment: Fragment() {
         const val TAG = "ChooserFragment"
     }
 
-
+    private var choosePhotoHelper: ChoosePhotoHelper? = null
     private val viewModel by activityViewModels<MainViewModel>()
 
     override fun onCreateView(
@@ -29,8 +32,39 @@ class HomeFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        viewModel.setAppbarTitle("")
+        viewModel.setAllowGoBack(false)
+        viewModel.setShowSettingsIcon(true)
+        viewModel.setShowLinkIcon(false)
+        viewModel.fragmentTag = TAG
+
         view.findViewById<WebView>(R.id.google_maps).loadUrl("https://mapsdirections.io")
 
+        view.findViewById<Button>(R.id.camera_button).setOnClickListener {
+            choosePhotoHelper?.takePhoto()
+            requestPermissions(arrayOf(android.Manifest.permission.CAMERA), 101)
+        }
 
+        choosePhotoHelper = ChoosePhotoHelper.with(this)
+            .asFilePath()
+            .withState(savedInstanceState)
+            .build {
+                viewModel.setImageFilePath(it.orEmpty())
+            }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        choosePhotoHelper?.onActivityResult(requestCode, resultCode, data)
+    }
+
+    override fun onRequestPermissionsResult(rqCode: Int, perms: Array<String>, grantRes: IntArray) {
+        super.onRequestPermissionsResult(rqCode, perms, grantRes)
+        choosePhotoHelper?.onRequestPermissionsResult(rqCode, perms, grantRes)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        choosePhotoHelper?.onSaveInstanceState(outState)
+        super.onSaveInstanceState(outState)
     }
 }
--- app/src/main/java/com/spin_cake_con/MainActivity.kt
+++ app/src/main/java/com/spin_cake_con/MainActivity.kt
@@ -1,13 +1,32 @@
 package com.spin_cake_con
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var camera_button: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val requestPermissionLauncher =
+            registerForActivityResult(
+                ActivityResultContracts.RequestPermission()
+            ) { isGranted: Boolean ->
+                if (isGranted) {
+                    // Permission is granted. Continue the action or workflow in your
+                    // app.
+                } else {
+                    // Explain to the user that the feature is unavailable because the
+                    // feature requires a permission that the user has denied. At the
+                    // same time, respect the user's decision. Don't link to system
+                    // settings in an effort to convince the user to change their
+                    // decision.
+                }
+            }
+
+        requestPermissionLauncher.launch(Manifest.permission.CAMERA)
     }
 }
