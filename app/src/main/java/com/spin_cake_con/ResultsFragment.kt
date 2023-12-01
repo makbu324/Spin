7c3b0540c7b9aed89ca6aa62d3f720f6f347bbe7
chrisrelyea <crelyea@bu.edu>
2023-11-30 19:15:42 -0500

    Removed Youtube search

--- app/src/main/java/com/spin_cake_con/ResultsFragment.kt
+++ app/src/main/java/com/spin_cake_con/ResultsFragment.kt
@@ -65,97 +65,10 @@ class ResultsFragment : Fragment() {
             offscreenPageLimit = pagesAdapter.count - 1
         }
 
-        val youTubePlayerView = view.findViewById<YouTubePlayerView>(R.id.youtube_player_view)
 
-        Thread.sleep(2000)
-        lifecycle.addObserver(youTubePlayerView)
-        youTubePlayerView.addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
-            override fun onReady(youTubePlayer: YouTubePlayer) {
-                val videoId = viewModel.youtube_id
-                youTubePlayer.loadVideo(videoId, 0f)
-            }
-        })
-
-        //Dialog for showing album
-        val child: View = layoutInflater.inflate(R.layout.suggested_album_dialog, null)
-        val img: ImageView = child.findViewById(R.id.possible_album)
-        if (viewModel.image_album_cover != "") {
-            img.getLayoutParams().height = 500
-            img.getLayoutParams().width = 500
-            img.requestLayout()
-            Glide.with(child).load(viewModel.image_album_cover).into(img)
-
-
-        }
-        view.findViewById<EditText>(R.id.hihihi).setText(viewModel.url_thing)
-
-        val builder: AlertDialog.Builder = AlertDialog.Builder(view.context)
-        builder
-            .setTitle(viewModel.url_thing)
-            .setMessage(viewModel.album + " - by " + viewModel.artist)
-            .setView(child)
-            .setPositiveButton("Positive") { dialog, which ->
-                // Do something.
-            }
-            .setNegativeButton("Negative") { dialog, which ->
-                // Do something else.
-            }
-
-        val dialog: AlertDialog = builder.create()
-        dialog.show()
 
 
     }
 
-    override fun onCreateContextMenu(menu: ContextMenu, v: View, menuInfo: ContextMenu.ContextMenuInfo?) {
-        super.onCreateContextMenu(menu, v, menuInfo)
-        val webView = getCurrentWebView()
-        val result = webView.hitTestResult
-        val url = result.extra
-        var link = url
-        var isLink = false
-        val filename = URLUtil.guessFileName(url, null, null)
 
-        if ((result.type == HitTestResult.IMAGE_TYPE ||
-                    result.type == HitTestResult.SRC_IMAGE_ANCHOR_TYPE) &&
-            URLUtil.isNetworkUrl(url)
-        ) {
-            Log.d(TAG, "Long clicked image $url")
-            menu.add(0, 1, 0, "R.string.save_image").setOnMenuItemClickListener {
-                Log.d(TAG, "Downloading image $filename from $url")
-                AdvancedWebView.handleDownload(context, url, filename)
-                true
-            }
-
-            val message = Handler().obtainMessage()
-            webView.requestFocusNodeHref(message)
-            val maybeLink = message.data.getString("url")
-            maybeLink?.let {
-                Log.d(TAG, "Found link on image $maybeLink")
-                isLink = true
-                link = maybeLink
-            }
-        }
-        if (isLink || (result.type == HitTestResult.SRC_ANCHOR_TYPE && URLUtil.isNetworkUrl(link))) {
-            Log.d(TAG, "Long clicked link $link")
-            menu.add(0, 2, 0, "R.string.open_external_browser").setOnMenuItemClickListener {
-                openBrowser(link)
-                true
-            }
-        }
-    }
-
-    fun openBrowser() {
-        openBrowser(getCurrentWebView().url)
-    }
-
-    fun openBrowser(url: String?) {
-        val webpage: Uri = Uri.parse(url)
-        val intent = Intent(Intent.ACTION_VIEW, webpage)
-        startActivity(intent)
-    }
-
-    fun getCurrentWebView(): WebView {
-        return pagesAdapter.getViewPage(viewPager.currentItem)
-    }
 }
